5db593bbb200a98dc375ab8d1b3b78f8a104c713
do0fusz@hotmail.com <do0fusz@hotmail.com>
2014-01-25 16:14:13 +0100

    finished execercices_3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-	
+
+	let(:base_title) { "Ruby on Rails sample app" }
+
 	describe "Home page" do
 		it "should have the content 'Sample App' "  do
 			visit '/static_pages/home'
 		    expect(page).to have_content('Sample App')			
 		end
-		it "should have the right title 'home page' "do
+		it "should have the right title 'home'"do
 			visit '/static_pages/home'
 			expect(page).to have_title('Home')
 		end
@@ -32,6 +34,17 @@ describe "Static Pages" do
 		it "should have the right title 'about' to" do
 			visit '/static_pages/about'
 			expect(page).to have_title('About')
+		end
+	end
+
+	describe "contact page" do
+		it "should have the content 'Contact' " do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the right title 'Contact' " do
+			visit '/static_pages/contact'
+			expect(page).to have_title('Contact')
 		end
 	end
 end
